0e370ae3cd96dd9d07115c84424f73fffe26cbf4
Prateek Chokse <prateek.chokse@live.com>
2018-05-10 02:34:48 +0530

    Updated comic books controller to use Repository Design Pattern

--- ComicBookGallery/Models/ComicBook.cs
+++ ComicBookGallery/Models/ComicBook.cs
@@ -7,7 +7,7 @@ namespace ComicBookGallery.Models
 {
     public class ComicBook
     {
-        public int Td { get; set; }
+        public int Id { get; set; }
         public string SeriesTitle { get; set; }
         public int IssueNumber { get; set; }
         public string DescriptionHtml { get; set; }
